35bac92fb0c79af0806cc26a183b1533a2d71900
Kavipiriya Thamilmaran <P2662650@my365.dmu.ac.uk>
2023-03-12 20:07:30 +0000

    Added testing for sales class

---
+++ Testing2/tstSales.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsSales AnSales = new clsSales();
+            //test to see that it exists 
+            Assert.IsNotNull(AnSales);
+        }
+    }
+
+    class clsSales
+    {
+    }
+}
