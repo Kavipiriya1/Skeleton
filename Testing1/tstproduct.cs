ad90f25fd317702a44b4b8f0f396947e1b409e09
Pragnesh Jaendra <P2664813@my365.dmu.ac.uk>
2023-03-13 14:11:03 +0000

    Added testing for product class

---
+++ Testing1/tstproduct.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstproduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsproduct Anproduct = new clsproduct();
+            //test to see that it exist
+            Assert.IsNotNull(Anproduct);
+        }
+
+    }
+}
